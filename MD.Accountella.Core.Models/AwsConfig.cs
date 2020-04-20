3fbba076efc04ba99ad31f73d70b2d59cc3aa817
Meghnath Das <MeghnathDas@eurofins.com>
2020-04-20 12:49:33 +0530

    Application architechural changes #3

---
+++ MD.Accountella.Core.Models/AwsConfig.cs
@@ -0,0 +1,14 @@
+﻿/// <summary>
+/// Author: Meghnath Das
+/// Description: Model for AWS configuration
+/// URL: http://meghnathdas.github.io/
+/// </summary>
+namespace MD.Accountella.Core.Models
+{
+    public class AwsConfig
+    {
+        public string AccessKeyID { get; set; }
+        public string AccessKey { get; set; }
+        public string Region { get; set; }
+    }
+}
