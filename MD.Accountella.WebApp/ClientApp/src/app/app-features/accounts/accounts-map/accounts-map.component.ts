736085d032ec3dca806bb70e3cd358d5ba9d859f
Meghnath Das <MeghnathDas@eurofins.com>
2020-05-10 16:04:04 +0530

    Design issue fixed - 3

--- MD.Accountella.WebApp/ClientApp/src/app/app-features/accounts/accounts-map/accounts-map.component.ts
+++ MD.Accountella.WebApp/ClientApp/src/app/app-features/accounts/accounts-map/accounts-map.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, ViewChildren, QueryList, AfterViewChecked } from '@angular/core';
+import {
+  Component, OnInit, ViewChildren, QueryList, AfterViewChecked,
+  ViewChild, ElementRef, AfterViewInit, HostListener
+} from '@angular/core';
 import { TitleService } from 'src/app/core';
 import { AccountMapService } from '../services/account-map/account-map.service';
 import { Category } from '../../models';
@@ -16,12 +19,36 @@ export class AccountsMapComponent implements OnInit, AfterViewChecked {
   caption = 'Account Mappings';
   accountHeads: Category[];
   @ViewChildren(ClrTabLink) headTabs: QueryList<ClrTabLink>;
+  @ViewChild('accTabContainer', { static: true }) accTabContainer: ElementRef;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.setTabLinks();
+  }
 
   constructor(private titleServ: TitleService, private accMapServ: AccountMapService) { }
   ngAfterViewChecked(): void {
     if (this.headTabs.first && this.autoTabSelectionRequired) {
       this.autoTabSelectionRequired = false;
       this.headTabs.toArray()[this.tabIndexToSelect].activate();
+      this.setTabLinks();
+    }
+  }
+  private setTabLinks() {
+    if (!this.headTabs || !this.headTabs.first) { return; }
+    this.headTabs.forEach(tb => tb.inOverflow = false);
+    const pElm = this.headTabs.first.el.nativeElement.parentElement.parentElement;
+    pElm.hidden = true;
+    const maxPermissibleTabWidth = this.accTabContainer.nativeElement.offsetWidth - 150;
+    pElm.hidden = false;
+
+    while (this.headTabs
+      .filter(itm => itm.inOverflow === undefined || itm.inOverflow === false)
+      .reduce((total, tab) => total + tab.el.nativeElement.offsetWidth, 0) >
+      maxPermissibleTabWidth) {
+      const avTab =
+        this.headTabs.filter(itm => itm.inOverflow === undefined || itm.inOverflow === false).reverse()[0];
+      avTab.inOverflow = true;
     }
   }
   ngOnInit(): void {
